59871904e8df46464746cbd8e35a32f997e6633e
aram.dani1996a <aram.dani1996a@gmail.com>
2025-02-01 00:21:12 +0400

    Sum

---
+++ ConsoleApp36/ConsoleApp36/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+           Console.WriteLine("Sum = "+calculator.sum(1, 2));
+        }
+    }
+    class Calculator
+    {
+        public int sum(int a, int b)
+        { 
+        return a + b;
+        }
+    }
+}
